f220dcd272c7f7b22a78070a5731f249d4eb05d0
Joelma <joelma.sp@outlook.com.br>
2021-07-06 11:35:09 -0300

    Developed person create operation

--- src/main/java/one/digitalinnovation/personapi/entity/Phone.java
+++ src/main/java/one/digitalinnovation/personapi/entity/Phone.java
@@ -1,7 +1,10 @@
 package one.digitalinnovation.personapi.entity;
 
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import one.digitalinnovation.personapi.enums.PhoneType;
 
 import javax.persistence.*;
